db432caa2747bfd21543b4fbb5b4d5c0576d35c1
OLEG GLAUBERMAN <OLEGGL@rf.local>
2018-08-07 14:11:05 +0300

    Added debug token for confd

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import { environment } from './../../../../environments/environment';
+
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created by <b><a href="http://www.rafael.co.il/" target="_blank">Rafael</a></b> 2018</span>
+    <span class="created-by" title={{token}}>Created by <b><a href="http://www.rafael.co.il/" target="_blank">Rafael</a></b> 2018</span>
   `,
 })
 export class FooterComponent {
+  token = environment.info.token;
 }
